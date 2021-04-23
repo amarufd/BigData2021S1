05938a174a7113c1fe23a9fc9abede3861508f1a
Amaru f <amarud@gmail.com>
2021-04-23 23:35:00 +0000

    clase de tipo de datos y comienzo de variables

---
+++ tipodadatos_y_operadores.R
@@ -0,0 +1,39 @@
+
+
+
+# esto es un comentario
+
+# Enteros
+1
+4
+45
+-23432
+355
+-12
+0
+
+# Doubles
+1.45
+232.3534
+-0.1434
+.239238
+
+# Boleanos
+TRUE
+T
+FALSE
+F
+
+# String y Caracteres
+"a"
+"hola mundo"
+"hoy fue un dia \"soleado\""
+"me gusta el sol"
+
+
+
+
+# creando la variable de nombre hola
+hola <- "pastito"
+hola <- "oveja"
+
