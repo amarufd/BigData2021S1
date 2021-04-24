b75d13bd1bd67ed649144404878a95b2f231fa56
Amaru f <amarud@gmail.com>
2021-04-24 15:09:11 +0000

    se agrega la clase de operaciones (asignacion, comparacion, logicas, aritmeticas)

--- tipodadatos_y_operadores.R
+++ tipodadatos_y_operadores.R
@@ -18,7 +18,7 @@
 -0.1434
 .239238
 
-# Boleanos
+# Booleanos
 TRUE
 T
 FALSE
@@ -30,10 +30,155 @@ F
 "hoy fue un dia \"soleado\""
 "me gusta el sol"
 
-
-
+########################################################################################################
+######################################## CREACION DE VARIABLES #########################################
+########################################################################################################
 
 # creando la variable de nombre hola
 hola <- "pastito"
 hola <- "oveja"
+
+variable1 <- "hola"
+c3po <- "hombre de lata" 
+r2d2 <- "el silvador"
+dos <- "algo"
+
+resultadodesuma <- 5 + 10
+elbooleano <- T
+otronumero <- 3.19383
+
+
+########################################################################################################
+############################################## OPERADORES ##############################################
+########################################################################################################
+
+
+##### Operaciones de Asignacion
+
+# asignando texto
+variable1 <- "hoy es una mañana soleada"
+
+# Asignando Entero
+variable1 <- 12423432
+
+# Asignando Double (racionales)
+variable1 <- 3444.32239387
+
+# Asignando Booleanos o logicos
+variable1 <- T
+
+
+##### Operaciones de Comparacion
+# Solo me dan respiuestas Logicas o booleanas
+
+4 < 3
+4 <= 4
+10 == 9
+"hola" == "hola"
+"uno" <= "dos" # !!!!! ALERTA ¡¡¡ Esto no esta bien
+TRUE == FALSE
+
+variable1 <- 4 < 3
+variable1 <- 4 <= 4
+variable1 <- 10 == 9
+
+a <- 4
+b <- 9
+
+variable1 <- a < b
+
+
+a <- 10
+b <- 9
+
+variable1 <- a < b
+
+
+variable1 <- "hola mundo"
+variable1 <- variable1 == "hola mundo"
+
+variable1 <- 5 != 5
+
+
+# Operaciones Logicas
+# Operadores de varoles logico
+
+# AND agregacio
+
+T && T
+T && F
+F && F
+
+# OR Exclusion
+
+T || T
+T || F
+F || F
+
+# Negacion
+
+!T
+!F
+
+## mas ejemplos de operaciones logicas
+
+a <- 10
+b <- 9
+c <- 8
+
+variable1 <- (a > b) || (b < c)
+variable1 <- (a > b) && (b < c)
+
+variable1 <- !((a > b) || (b < c))
+variable1 <- !((a > b) && (b < c))
+
+
+# Operadores Aritmeticos
+
+# suma
+5 + 7
+
+# multiplicacion
+5 * 12
+
+# resta
+6 - 20
+
+# division
+13 / 11
+
+# elevado
+2**3
+2^3
+
+# modulo
+# obtencion del resto en una division
+
+2 %% 2
+4 %% 3
+5 %% 3
+
+
+# como saber si un numero es par o impar
+
+
+32 %% 2
+53 %% 2
+5 %% 2
+34 %% 2
+361 %% 2
+6 %% 2
+3 %% 2
+43 %% 2
+
+
+a <- 4
+a %% 2 == 0
+
+a <- 3
+a %% 2 == 0
+
+
+
+
 
